d2fa18f706dd869330681eb4ad5460fd65c05cb4
Pietro907 <pietrop.puri@gmail.com>
2023-11-07 16:50:57 +0100

    add migration, model & controller

---
+++ app/Http/Controllers/Admin/CardsGamesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\CardsGames;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CardsGamesController extends Controller
+{
+    //
+}
